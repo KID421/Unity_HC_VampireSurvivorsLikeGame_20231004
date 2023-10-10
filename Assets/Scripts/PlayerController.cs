0eef2bd0e4ce2da582b88b98598a7cf605cc6dc2
KID <50666207+KID421@users.noreply.github.com>
2023-10-10 13:29:06 +0800

    Update 添加武器系統

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,58 @@ namespace KID.TopDwon
     /// </summary>
     public class PlayerController : MonoBehaviour
     {
-        
+        #region 資料
+        [SerializeField, Header("移動速度"), Range(0, 50)]
+        private float speed = 3.5f;
+
+        private string parRun = "開關跑步";
+        private Animator ani;
+        private Rigidbody2D rig;
+
+        private float inputHorizontal => Input.GetAxis("Horizontal");
+        private float inputVertical => Input.GetAxis("Vertical");
+        #endregion
+
+        #region 事件
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();
+            rig = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Move();
+            Flip();
+            UpdateAnimator();
+        }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 移動
+        /// </summary>
+        private void Move()
+        {
+            rig.velocity = new Vector2(inputHorizontal, inputVertical) * speed;
+        }
+
+        /// <summary>
+        /// 翻面
+        /// </summary>
+        private void Flip()
+        {
+            if (inputHorizontal > 0) transform.eulerAngles = Vector3.zero;
+            else if (inputHorizontal < 0) transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        /// <summary>
+        /// 更新跑步與待機動畫
+        /// </summary>
+        private void UpdateAnimator()
+        {
+            ani.SetBool(parRun, inputHorizontal != 0 || inputVertical != 0);
+        } 
+        #endregion
     }
 }
---
+++ Assets/Scripts/WeaponSystem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace KID.TopDwon
+{
+    /// <summary>
+    /// 武器系統：生成武器
+    /// </summary>
+    public class WeaponSystem : MonoBehaviour
+    {
+
+    }
+}
