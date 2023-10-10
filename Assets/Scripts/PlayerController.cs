4d3c63c3f2b7cce8c335e30e9db2c1c41a732678
KID <50666207+KID421@users.noreply.github.com>
2023-10-10 11:42:45 +0800

    Update 建立角色控制器

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace KID.TopDwon
+{
+    /// <summary>
+    /// Top Down 類型 2D 角色控制器：移動與動畫
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }
+}
