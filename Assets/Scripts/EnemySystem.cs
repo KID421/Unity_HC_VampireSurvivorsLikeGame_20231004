3f2184d39bf2f004a6daa1c5df6a76ec1200c85e
KID <50666207+KID421@users.noreply.github.com>
2023-10-13 13:07:34 +0800

    Update 添加敵人系統

--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -12,5 +12,35 @@ namespace KID
 
         private Transform pointPlayer;
         private string namePlayer = "女學生";
+
+        private void Awake()
+        {
+            pointPlayer = GameObject.Find(namePlayer).transform;
+        }
+
+        private void Update()
+        {
+            Move();
+            Flip();
+        }
+
+        /// <summary>
+        /// 移動
+        /// </summary>
+        private void Move()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pointPlayer.position, moveSpeed);
+        }
+
+        /// <summary>
+        /// 翻面：根據與玩家的 X 做比較
+        /// X > 玩家的 X，角度：0，0，0
+        /// X < 玩家的 X，角度：0，180，0
+        /// </summary>
+        private void Flip()
+        {
+            if (transform.position.x > pointPlayer.position.x) transform.eulerAngles = Vector3.zero;
+            else if (transform.position.x < pointPlayer.position.x) transform.eulerAngles = new Vector3(0, 180, 0);
+        }
     }
 }
