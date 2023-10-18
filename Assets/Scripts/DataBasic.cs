1e3cca351a0b436feccf9d7a747f22e390b66e12
KID <50666207+KID421@users.noreply.github.com>
2023-10-18 11:24:01 +0800

    Update 添加角色資料與敵人資料

---
+++ Assets/Scripts/DataBasic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 角色基本資料：血量、移動速度
+    /// </summary>
+    [CreateAssetMenu(menuName = "KID/Data Basic", fileName = "Data Basic")]
+    public class DataBasic : ScriptableObject
+    {
+        [Header("血量"), Range(0, 10000)]
+        public float hp;
+        [Header("移動速度"), Range(0, 10)]
+        public float speed;
+    }
+}
---
+++ Assets/Scripts/DataEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 敵人資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "KID/Data Enemy", fileName = "Data Enemy")]
+    public class DataEnemy : DataBasic
+    {
+        [Header("掉落經驗值機率"), Range(0, 1)]
+        public float expProbability;
+        [Header("經驗值物件")]
+        public GameObject expPrefab;
+        [Header("攻擊力"), Range(0, 10000)]
+        public float attack;
+    }
+}
